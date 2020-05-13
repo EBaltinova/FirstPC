3d4ed5e3a6f8a24d1d34731be7f18e1dbdaa21f0
Mira <ebaltinova@issart.com>
2020-05-13 13:39:32 +0600

    №2. Расстояние между двумя точками 1-3 пункты

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,15 +3,21 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		hello("world");
+		/*hello("world");
 		Square s = new Square(5);
 		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(5,6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b +" = " + r.area ());
+		*/
+		Point p1 = new Point(2,3);
+		Point p2 = new Point(5,7);
+		System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y +") = " + distance(p1,p2));
 	}
-	public static void hello(String somebody){
+	/*public static void hello(String somebody){
 	    System.out.println("Hi, " + somebody + "!");
+	} */
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
 	}
-
 }
