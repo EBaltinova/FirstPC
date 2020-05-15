cc7bc8602126073a6a4e614162fe0f2ff88b073a
ebaltinova <ebaltinova@issart.com>
2020-05-15 15:25:38 +0600

    Задание №3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -14,6 +14,10 @@ public class MyFirstProgram {
 		Point p2 = new Point (5,7);
 		System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y +") = " + p1.distance(p2));
 
+		Point p3 = new Point(7,6);
+		Point p4 = new Point (2,9);
+		System.out.println("Расстояние между точками (" + p3.x + "," + p3.y + ") и (" + p4.x + "," + p4.y +") = " + p3.distance(p4));
+
 	}
 
 }
