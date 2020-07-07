ade5cd090a958686934f6abd58edecad6d427a88
ebaltinova <ebaltinova@issart.com>
2020-07-07 12:40:46 +0600

    Задание №4 (наконец-то)!!!

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.addressbook;
 
-public class GroupData1 {
+public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
 
-    public GroupData1(String name, String header, String footer) {
+    public GroupData(String name, String header, String footer) {
         this.name = name;
         this.header = header;
         this.footer = footer;
